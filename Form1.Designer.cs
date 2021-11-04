74681fb440f58232538cac56ce37ebe3bd0a3968
Brennan Collins <BCOLLI7351@my.sullivan.edu>
2021-11-04 19:22:51 -0400

    Part one of second half of YouDoIt.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,8 @@ namespace CSC240_06_01_BedAndBreakfast_bpc
         {
             this.UxWelcomeLabel = new System.Windows.Forms.Label();
             this.UxRateLabel = new System.Windows.Forms.Label();
+            this.UxBelleAireCheckBox1 = new System.Windows.Forms.CheckBox();
+            this.UxLincolnCheckBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // UxWelcomeLabel
@@ -47,11 +49,33 @@ namespace CSC240_06_01_BedAndBreakfast_bpc
             // 
             this.UxRateLabel.AutoSize = true;
             this.UxRateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.UxRateLabel.Location = new System.Drawing.Point(335, 199);
+            this.UxRateLabel.Location = new System.Drawing.Point(329, 158);
             this.UxRateLabel.Name = "UxRateLabel";
             this.UxRateLabel.Size = new System.Drawing.Size(325, 51);
             this.UxRateLabel.TabIndex = 1;
             this.UxRateLabel.Text = "Check our rates";
+            // 
+            // UxBelleAireCheckBox1
+            // 
+            this.UxBelleAireCheckBox1.AutoSize = true;
+            this.UxBelleAireCheckBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.UxBelleAireCheckBox1.Location = new System.Drawing.Point(329, 274);
+            this.UxBelleAireCheckBox1.Name = "UxBelleAireCheckBox1";
+            this.UxBelleAireCheckBox1.Size = new System.Drawing.Size(320, 46);
+            this.UxBelleAireCheckBox1.TabIndex = 2;
+            this.UxBelleAireCheckBox1.Text = "Belle Aire Suite";
+            this.UxBelleAireCheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // UxLincolnCheckBox1
+            // 
+            this.UxLincolnCheckBox1.AutoSize = true;
+            this.UxLincolnCheckBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.UxLincolnCheckBox1.Location = new System.Drawing.Point(329, 341);
+            this.UxLincolnCheckBox1.Name = "UxLincolnCheckBox1";
+            this.UxLincolnCheckBox1.Size = new System.Drawing.Size(290, 46);
+            this.UxLincolnCheckBox1.TabIndex = 3;
+            this.UxLincolnCheckBox1.Text = "Lincoln Room";
+            this.UxLincolnCheckBox1.UseVisualStyleBackColor = true;
             // 
             // BaileysForm
             // 
@@ -59,6 +83,8 @@ namespace CSC240_06_01_BedAndBreakfast_bpc
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Yellow;
             this.ClientSize = new System.Drawing.Size(1022, 763);
+            this.Controls.Add(this.UxLincolnCheckBox1);
+            this.Controls.Add(this.UxBelleAireCheckBox1);
             this.Controls.Add(this.UxRateLabel);
             this.Controls.Add(this.UxWelcomeLabel);
             this.Name = "BaileysForm";
@@ -72,6 +98,8 @@ namespace CSC240_06_01_BedAndBreakfast_bpc
 
         private System.Windows.Forms.Label UxWelcomeLabel;
         private System.Windows.Forms.Label UxRateLabel;
+        private System.Windows.Forms.CheckBox UxBelleAireCheckBox1;
+        private System.Windows.Forms.CheckBox UxLincolnCheckBox1;
     }
 }
 
